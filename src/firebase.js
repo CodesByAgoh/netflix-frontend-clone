06e500a9b38b6e3e2f71eb7ea2eefd7834f35761
CodesByAgoh <96971154+CodesByAgoh@users.noreply.github.com>
2022-03-08 20:42:09 +0100

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDy9Nq0gFV0Gskit4GjTJzL76kGzA8hgIw",
+    authDomain: "netflix-clone-yktv.firebaseapp.com",
+    projectId: "netflix-clone-yktv",
+    storageBucket: "netflix-clone-yktv.appspot.com",
+    messagingSenderId: "834209422452",
+    appId: "1:834209422452:web:be416c9ef3a8b90f4e8521"
+  };
